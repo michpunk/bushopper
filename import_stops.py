5ab0d973d5916526e041ba1c06225b9d93947752
michpunk <d.adamsky@gmail.com>
2016-09-16 16:38:09 +0100

    a bit of cheating and interactive map.

--- import_stops.py
+++ import_stops.py
@@ -4,6 +4,7 @@ import sys
 import time
 from geojson import Feature, Point, FeatureCollection
 import geojson
+import json
 from get_geo_neighbours import get_stops_and_interchanges
 from process_routes import process_routes
 from add_interchanges import add_interchanges
@@ -44,6 +45,23 @@ def dijkstra(adj_lists, stops, neighb, source):
           prev[n] = u
     return dist
 
+def cheat_dijkstra(adj_lists, stops, neighb, source):
+  dist = {}
+  dist[source] = 0
+  for stop in neighb[source]:
+    dist[stop] = 0
+  d1 = set(adj_lists[source])
+  for stop in neighb[source]:
+    d1.update(adj_lists[stop])
+  for stop in d1:
+    if stop not in dist:
+      dist[stop] = 1;
+  for stop1 in d1:
+    for stop2 in adj_lists[stop1]:
+      if stop2 not in dist:
+        dist[stop2] = 2
+  return dist
+
 def json_dump(coord_file, dists):
   fn = coord_file
   Flist = []
@@ -60,15 +78,24 @@ def json_dump(coord_file, dists):
   with open('stops.geojson', 'w') as outfile:
     geojson.dump(FC, outfile)
 
+def dict_dump(dict_file, d):
+  with open(dict_file, 'w') as outfile:
+    outfile.write('var distDict = ')
+    json.dump(d, outfile)
+    outfile.write(';')
+
+
 def main(argv):
   (stops, neighb) = get_stops_and_interchanges(argv[1])
   adj_lists = process_routes(argv[2], stops)
   add_interchanges(stops, adj_lists, neighb)
   t = time.time()
-  d = dijkstra(adj_lists, stops, neighb, '2119')
+  #d = dijkstra(adj_lists, stops, neighb, '2196')
+  d = cheat_dijkstra(adj_lists, stops, neighb, '2196')
   elapsed = time.time() - t
   print "Elapsed time: " + str(elapsed)
-  json_dump('./bus_stops_coords.csv',d)
+  dict_dump('2196-cheat.js',d)
+  #json_dump('./bus_stops_coords.csv',d)
 
 if __name__ == "__main__":
   main(sys.argv)
